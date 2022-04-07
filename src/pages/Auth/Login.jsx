2b8007c967413780c8a90445ac014b05ef352a2d
Manoj Kumar <70743261+manojkumar8400@users.noreply.github.com>
2022-04-07 21:24:37 +0530

    some minor changes

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -5,25 +5,25 @@ const Login = () => {
 
     return (
         <>
-            <form class="login-main-container">
-                <div class="login-container">
-                    <h1 class="login-heading">Login</h1>
-                    <section class="mail-password-container">
+            <form className="login-main-container">
+                <div className="login-container">
+                    <h1 className="login-heading">Login</h1>
+                    <section className="mail-password-container">
                         <label for="/">Email address</label>
-                        <input class="mail-input" type="email" placeholder="abc@gmail.com" />
+                        <input className="mail-input" type="email" placeholder="abc@gmail.com" />
                         <label for="/">Password</label>
-                        <input class="pass-input" type="password" placeholder="*************" />
+                        <input className="pass-input" type="password" placeholder="*************" />
                     </section>
-                    <section class="forget-password-section">
+                    <section className="forget-password-section">
                         <label for="/">
-                            <input class="checkbox-inpt" type="checkbox" />
+                            <input className="checkbox-inpt" type="checkbox" />
                             Remember me</label>
-                        <Link to="/Forget" class="forget-pass-link">Forget Password?</Link>
+                        <Link to="/Forget" className="forget-pass-link">Forget Password?</Link>
                     </section>
                     <Link to="/AllProducts">
-                        <button class="login-btn">Login</button><br />
+                        <button className="login-btn">Login</button><br />
                     </Link>
-                    <Link class="create-account-link" to="/Signup">Create New Account</Link>
+                    <Link className="create-account-link" to="/Signup">Create New Account</Link>
                 </div>
             </form>
         </>
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -5,29 +5,29 @@ const Signup = () => {
 
     return (
         <>
-            <form class="signup-main-container">
-                <div class="signup-container">
-                    <h2 class="signup-heading">Signup</h2>
-                    <label class="email-add-txt">Email address</label>
-                    <input class="email-input input" type="email" placeholder="abc@gmail.com" />
-                    <label class="email-add">First name</label>
-                    <input class="email-input input" type="text" placeholder="Enter First Name" />
-                    <label class="email-add">Last name</label>
-                    <input class="email-input input" type="text" placeholder="Enter Last Name" />
-                    <label class="password">Password</label>
-                    <input class="email-input input" type="password" placeholder="Enter Password" />
-                    <label class="password">Confirm Password</label>
-                    <input class="email-input input" type="password" placeholder="Confirm Password" /><br />
-                    <div class="checkbox-item">
-                        <input class="checkbox-input" type="checkbox" />
-                        <label class="checkbox-txt">I accept all Terms & Conditions</label><br />
+            <form className="signup-main-container">
+                <div className="signup-container">
+                    <h2 className="signup-heading">Signup</h2>
+                    <label className="email-add-txt">Email address</label>
+                    <input className="email-input input" type="email" placeholder="abc@gmail.com" />
+                    <label className="email-add">First name</label>
+                    <input className="email-input input" type="text" placeholder="Enter First Name" />
+                    <label className="email-add">Last name</label>
+                    <input className="email-input input" type="text" placeholder="Enter Last Name" />
+                    <label className="password">Password</label>
+                    <input className="email-input input" type="password" placeholder="Enter Password" />
+                    <label className="password">Confirm Password</label>
+                    <input className="email-input input" type="password" placeholder="Confirm Password" /><br />
+                    <div className="checkbox-item">
+                        <input className="checkbox-input" type="checkbox" />
+                        <label className="checkbox-txt">I accept all Terms & Conditions</label><br />
                     </div>
-                    <button class="submit-btn">
+                    <button className="submit-btn">
                         <Link to="/AllProducts">
                             Create New Account
                         </Link>
                     </button>
-                    <label class="login-link">Already a member?
+                    <label className="login-link">Already a member?
                         <Link to="/Login"><strong>Login</strong></Link>
                     </label>
                 </div>
